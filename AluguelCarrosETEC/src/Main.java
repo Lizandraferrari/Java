505f967abeae02b18bca330e59dd7a4074146031
lizandra <lizandra.ferrari@hotmail.com>
2022-09-08 14:43:02 -0300

    Exercicio aluguel de carro

--- AluguelCarrosETEC/src/Main.java
+++ AluguelCarrosETEC/src/Main.java
@@ -1,5 +1,4 @@
 import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
 /*A loja Velox, locadora de carros, precisa de um programa que ajude a cobrar os serviços
@@ -9,7 +8,6 @@ public class Main {
     Quantidade de dias da locação
     Valor da diária da locação
     Quantidade de Km percorridos
-
 Calcular o preço total a pagar na locação com base na quantidade de dias e valor da diária,
  adicionando R$0,79 por Km rodado.
 O programa deve exibir na tela o modelo do carro e o valor total a pagar.
@@ -22,9 +20,24 @@ O programa deve exibir na tela o modelo do carro e o valor total a pagar.
         Scanner input = new Scanner(System.in);
         String modelo;
         int dias;
-        float diaria;
-        float distancia;
-        float resultado;
-        
+        double diaria;
+        double distancia;
+        double resultado;
+
+        System.out.println("Insira o modelo do carro alugado: ");
+        modelo = input.next();
+        System.out.println("Insira o número de dias que foi alugado o veiculo: ");
+        dias = input.nextInt();
+        System.out.println("Insira o valor da diária do veículo: ");
+        diaria = input.nextDouble();
+        System.out.println("Insira o número de KM rodados: ");
+        distancia = input.nextDouble();
+        resultado = ((double)dias * diaria + distancia * 0.79);
+
+        //System.out.println("\nQuem alugou o carro "+modelo+" precisa pagar R$");
+        //System.out.format("%.2f",resultado);
+        System.out.println("\nQuem alugou o carro "+modelo +" precisa pagar R$"
+                +String.format("%.2f",resultado));
+
     }
 }
